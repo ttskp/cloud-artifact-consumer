b12bb342d5c3d7c255df49fc33a519f55d6ae15b
Stefan Boshe-Plois <stefan.boshe-plois@tt-s.com>
2019-04-11 14:55:25 +0200

    Added TODOs for next steps

--- copy_files/function.py
+++ copy_files/function.py
@@ -1,3 +1,9 @@
-# TODO: Implement copying files to target bucket using the incoming pregined urls
+# TODO: Implement copying files to target bucket using the incoming presigned urls
 def handler(event, context):
     print(event)
+
+# TODO: Iterate queue messages in event and foreach:
+#   1. Get the presigned url
+#   2. Get target object path
+#   3. Copy file from presigned url
+#   4. Put object into bucket
