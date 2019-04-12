f8684f7c83ca6528773bc2981b8afabcca40d460
OrdonioSa <sascha.ordonio@tt-s.com>
2019-04-12 17:03:01 +0200

    first version of copy_files/function; added permission PutObject in template; added requirements

--- copy_files/function.py
+++ copy_files/function.py
@@ -1,6 +1,27 @@
+import json
+import os
+
+import boto3
+import requests
+
+
 # TODO: Implement copying files to target bucket using the incoming presigned urls
 def handler(event, context):
     print(event)
+
+    presigned_url = json.loads(event["Records"][0]["body"])["ArtifactUrl"]
+
+    s3 = boto3.client('s3')
+
+    requested_object_as_stream = requests.get(presigned_url, stream=True)
+    file_object_from_req = requested_object_as_stream.raw
+    req_data = file_object_from_req.read()
+
+    # Do the actual upload to s3
+    artifacts_bucket_name = os.environ["ARTIFACTS_BUCKET"]
+    s3_filename = "template.yaml"   # test param
+    s3.put_object(Bucket=artifacts_bucket_name, Key=s3_filename, Body= req_data)
+
 
 # TODO: Iterate queue messages in event and foreach:
 #   1. Get the presigned url
