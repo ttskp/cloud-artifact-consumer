261aecf99e9382dc46040cbd24e85259d1d5ebaf
OrdonioSa <sascha.ordonio@tt-s.com>
2019-04-15 12:10:14 +0200

    added get key from message

--- copy_files/function.py
+++ copy_files/function.py
@@ -5,11 +5,11 @@ import boto3
 import requests
 
 
-# TODO: Implement copying files to target bucket using the incoming presigned urls
 def handler(event, context):
     print(event)
 
     presigned_url = json.loads(event["Records"][0]["body"])["ArtifactUrl"]
+    s3_filename = json.loads(event["Records"][0]["body"])["Key"]
 
     s3 = boto3.client('s3')
 
@@ -17,14 +17,6 @@ def handler(event, context):
     file_object_from_req = requested_object_as_stream.raw
     req_data = file_object_from_req.read()
 
-    # Do the actual upload to s3
+    # Do the actual upload to s3 Todo: multiple migration (?)
     artifacts_bucket_name = os.environ["ARTIFACTS_BUCKET"]
-    s3_filename = "template.yaml"   # test param
     s3.put_object(Bucket=artifacts_bucket_name, Key=s3_filename, Body=req_data)
-
-
-# TODO: Iterate queue messages in event and foreach:
-#   1. Get the presigned url
-#   2. Get target object path
-#   3. Copy file from presigned url
-#   4. Put object into bucket
