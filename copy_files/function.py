480b67c5466aed28716084785a705a306c7d03ad
OrdonioSa <sascha.ordonio@tt-s.com>
2019-04-15 15:15:54 +0200

    refactored copy_files.function; fixed tests of copy_files.function

--- copy_files/function.py
+++ copy_files/function.py
@@ -8,14 +8,23 @@ import requests
 def handler(event, context):
     print(event)
 
-    presigned_url = json.loads(event["Records"][0]["body"])["ArtifactUrl"]
-    s3_filename = json.loads(event["Records"][0]["body"])["Key"]
+    for record in event["Records"]:
+        presigned_url = json.loads(record["body"])["ArtifactUrl"]
 
+        file_name = json.loads(record["body"])["Key"]
+        file_data = download_file_data(presigned_url)
+
+        upload_file_to_bucket(file_data, file_name)
+
+
+def download_file_data(presigned_url):
     requested_object_as_stream = requests.get(presigned_url, stream=True)
-    file_object_from_req = requested_object_as_stream.raw
-    req_data = file_object_from_req.read()
+    file_object_from_request = requested_object_as_stream.raw
+    file_data = file_object_from_request.read()
+    return file_data
 
-    # Do the actual upload to s3
+
+def upload_file_to_bucket(file_data, s3_filename):
     artifacts_bucket_name = os.environ["ARTIFACTS_BUCKET"]
     s3 = boto3.client('s3')
-    s3.put_object(Bucket=artifacts_bucket_name, Key=s3_filename, Body=req_data)
+    s3.put_object(Bucket=artifacts_bucket_name, Key=s3_filename, Body=file_data)
--- tests/unit/copy_files/test_copy_files.py
+++ tests/unit/copy_files/test_copy_files.py
@@ -8,17 +8,23 @@ from moto import mock_s3
 
 from copy_files import function
 
-TEST_SIGNED_URL = "https://artifactUrl{}/"
+TEST_OBJECT_KEY = "item{}"
+
+TEST_SIGNED_URL = "http://artifacturl{}/"
 
 
-@responses.activate
 @mock_s3
+@responses.activate
 def test_copy_files_to_s3(mocker, event_builder):
     message_count = 10
+
     event = event_builder(message_count)
     given_signed_url_responses(message_count)
     given_bucket(mocker)
+
     function.handler(event, None)
+
+    assert_files_in_bucket(message_count)
 
 
 @pytest.fixture
@@ -28,7 +34,7 @@ def event_builder():
         for i in range(message_count):
             records.append({
                 "body": json.dumps({
-                    "Key": f"item{i}",
+                    "Key": TEST_OBJECT_KEY.format(i),
                     "ArtifactUrl": TEST_SIGNED_URL.format(i)
                 })
             })
@@ -41,13 +47,27 @@ def event_builder():
 
 def given_signed_url_responses(message_count):
     for i in range(message_count):
-        responses.add(method='GET', url=TEST_SIGNED_URL.format(i), body="abcd", stream=True)
-        print(responses)
-        print(TEST_SIGNED_URL.format(i))
+        responses.add(
+            method='GET',
+            url=TEST_SIGNED_URL.format(i),
+            body="abcd",
+            stream=True
+        )
 
 
 def given_bucket(mocker):
     mocker.patch.dict(os.environ, {"ARTIFACTS_BUCKET": "test-bucket"})
 
     mocked_client = boto3.client('s3')
-    mocked_client.create_bucket(Bucket='test-bucket')
+    mocked_client.create_bucket(Bucket="test-bucket")
+
+
+def assert_files_in_bucket(object_count):
+    s3 = boto3.resource('s3')
+    test_bucket = s3.Bucket("test-bucket")
+    keys_in_bucket = [obj.key for obj in test_bucket.objects.all()]
+
+    assert len(keys_in_bucket) == object_count
+
+    for i in range(object_count):
+        assert TEST_OBJECT_KEY.format(i) in keys_in_bucket
