c076989729d4db75bfad30147661d76adca04f95
Stefan Boshe-Plois <stefan.boshe-plois@tt-s.com>
2019-05-02 15:24:03 +0200

    Added replacement of distribution bucket name with target bucket name

--- copy_files/function.py
+++ copy_files/function.py
@@ -16,6 +16,10 @@ def handler(event, context):
         file_name = body["ArtifactKey"]
         file_data = download_file_data(presigned_url)
 
+        if is_template(file_name, file_data):
+            file_data = replace_bucket_name_in_template(file_data)
+
+        # print(file_data.decode("utf-8"))
         upload_file_to_bucket(file_data, file_name)
 
 
@@ -30,3 +34,28 @@ def upload_file_to_bucket(file_data, s3_filename):
     artifacts_bucket_name = os.environ["ARTIFACTS_BUCKET"]
     s3 = boto3.client('s3')
     s3.put_object(Bucket=artifacts_bucket_name, Key=s3_filename, Body=file_data)
+
+
+def replace_bucket_name_in_template(file_data):
+    target_bucket = os.environ["ARTIFACTS_BUCKET"]
+    distributor_bucket = os.environ["DISTRIBUTOR_BUCKET"]
+    return file_data \
+        .replace(b"s3://%b" % distributor_bucket.encode(),
+                 b"s3://%b" % target_bucket.encode()) \
+        .replace(b"https://%b" % distributor_bucket.encode(),
+                 b"https://%b" % target_bucket.encode()) \
+        .replace(b"Default: %b" % distributor_bucket.encode(),
+                 b"Default: %b" % target_bucket.encode())
+
+
+def is_template(file_name, file_data):
+    return is_yaml_file(file_name) and has_template_header(file_data)
+
+
+def is_yaml_file(file_name):
+    return file_name.endswith(".yaml") or file_name.endswith(".yml")
+
+
+def has_template_header(file_data):
+    file_content = file_data.decode("utf-8")
+    return file_content.startswith("AWSTemplateFormatVersion")
--- tests/unit/copy_files/test_copy_files.py
+++ tests/unit/copy_files/test_copy_files.py
@@ -8,10 +8,105 @@ from moto import mock_s3
 
 from copy_files import function
 
+TEST_CONSUMER_BUCKET_NAME = "test-bucket"
+
 TEST_OBJECT_KEY = "item{}"
 
 TEST_SIGNED_URL = "http://artifacturl{}/"
 
+TEST_TEMPLATE = """AWSTemplateFormatVersion: '2010-09-09'
+Description: Test Template.
+Globals:
+  Function:
+    Timeout: 3
+Parameters:
+  TestParameter:
+    Default: bla
+    Type: String
+  TemplatesBucket:
+    Type: String
+    Default: dist-bucket
+Resources:
+  SomeBucket:
+    Properties:
+      BucketName:
+        SomeBucketName
+    Type: AWS::S3::Bucket
+  CopyFunction:
+    Properties:
+      CodeUri: s3://dist-bucket/someFunction
+      Environment:
+        Variables:
+          BUCKET:
+            Ref: SomeBucket
+      Handler: function.handler
+      Role:
+        Fn::GetAtt:
+        - CopyRole
+        - Arn
+      Runtime: python3.6
+    Type: AWS::Serverless::Function
+  SecondFunction:
+    Properties:
+      CodeUri: https://dist-bucket/someFunction
+      Environment:
+        Variables:
+          BUCKET:
+            Ref: SomeBucket
+      Handler: function.handler
+      Role:
+        Fn::GetAtt:
+        - CopyRole
+        - Arn
+      Runtime: python3.6
+    Type: AWS::Serverless::Function"""
+
+TRANSFORMED_TEST_TEMPLATE = """AWSTemplateFormatVersion: '2010-09-09'
+Description: Test Template.
+Globals:
+  Function:
+    Timeout: 3
+Parameters:
+  TestParameter:
+    Default: bla
+    Type: String
+  TemplatesBucket:
+    Type: String
+    Default: test-bucket
+Resources:
+  SomeBucket:
+    Properties:
+      BucketName:
+        SomeBucketName
+    Type: AWS::S3::Bucket
+  CopyFunction:
+    Properties:
+      CodeUri: s3://test-bucket/someFunction
+      Environment:
+        Variables:
+          BUCKET:
+            Ref: SomeBucket
+      Handler: function.handler
+      Role:
+        Fn::GetAtt:
+        - CopyRole
+        - Arn
+      Runtime: python3.6
+    Type: AWS::Serverless::Function
+  SecondFunction:
+    Properties:
+      CodeUri: https://test-bucket/someFunction
+      Environment:
+        Variables:
+          BUCKET:
+            Ref: SomeBucket
+      Handler: function.handler
+      Role:
+        Fn::GetAtt:
+        - CopyRole
+        - Arn
+      Runtime: python3.6
+    Type: AWS::Serverless::Function"""
 
 @mock_s3
 @responses.activate
@@ -25,6 +120,46 @@ def test_copy_files_to_s3(mocker, event_builder):
     function.handler(event, None)
 
     assert_files_in_bucket(message_count)
+
+
+@mock_s3
+@responses.activate
+def test_transform_template_before_upload(mocker, template_object_event):
+    given_bucket(mocker)
+    given_bucket(mocker, env_variable="DISTRIBUTOR_BUCKET", bucket_name="dist-bucket")
+
+    given_signed_url_responses_for_templates(3)
+    function.handler(template_object_event, None)
+
+    assert_template_has_content("packaged.yaml", TRANSFORMED_TEST_TEMPLATE, TEST_CONSUMER_BUCKET_NAME)
+    assert_template_has_content("packaged.yml", TRANSFORMED_TEST_TEMPLATE, TEST_CONSUMER_BUCKET_NAME)
+    assert_template_has_content("packaged.json", TEST_TEMPLATE, TEST_CONSUMER_BUCKET_NAME)
+
+
+@pytest.fixture
+def template_object_event():
+    return {
+        "Records": [
+            {
+                "body": json.dumps({
+                    "ArtifactKey": "packaged.yaml",
+                    "ArtifactUrl": TEST_SIGNED_URL.format(0)
+                })
+            },
+            {
+                "body": json.dumps({
+                    "ArtifactKey": "packaged.yml",
+                    "ArtifactUrl": TEST_SIGNED_URL.format(1)
+                })
+            },
+            {
+                "body": json.dumps({
+                    "ArtifactKey": "packaged.json",
+                    "ArtifactUrl": TEST_SIGNED_URL.format(2)
+                })
+            }
+        ]
+    }
 
 
 @pytest.fixture
@@ -55,19 +190,43 @@ def given_signed_url_responses(message_count):
         )
 
 
-def given_bucket(mocker):
-    mocker.patch.dict(os.environ, {"ARTIFACTS_BUCKET": "test-bucket"})
+def given_signed_url_responses_for_templates(message_count):
+    for i in range(message_count):
+        responses.add(
+            method="GET",
+            url=TEST_SIGNED_URL.format(i),
+            body=TEST_TEMPLATE,
+            stream=True
+        )
+
+
+def given_bucket(mocker, env_variable="ARTIFACTS_BUCKET", bucket_name=TEST_CONSUMER_BUCKET_NAME):
+    mocker.patch.dict(os.environ, {env_variable: bucket_name})
 
     mocked_client = boto3.client('s3')
-    mocked_client.create_bucket(Bucket="test-bucket")
+    mocked_client.create_bucket(Bucket=TEST_CONSUMER_BUCKET_NAME)
+
+
+def given_distributor_bucket(mocker, bucket_name):
+    mocker.patch.dict(os.environ, {"DISTRIBUTOR_BUCKET": bucket_name})
 
 
 def assert_files_in_bucket(object_count):
     s3 = boto3.resource('s3')
-    test_bucket = s3.Bucket("test-bucket")
+    test_bucket = s3.Bucket(TEST_CONSUMER_BUCKET_NAME)
     keys_in_bucket = [obj.key for obj in test_bucket.objects.all()]
 
     assert len(keys_in_bucket) == object_count
 
     for i in range(object_count):
         assert TEST_OBJECT_KEY.format(i) in keys_in_bucket
+
+
+
+
+def assert_template_has_content(object_key, expected_template, bucket_name):
+    s3 = boto3.resource('s3')
+    saved_template_stream = s3.Object(key=object_key, bucket_name=bucket_name).get()['Body'].read()
+    saved_template = saved_template_stream.decode("utf-8")
+
+    assert expected_template == saved_template
