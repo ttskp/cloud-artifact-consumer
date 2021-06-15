3701cacb6f067c787df68213250caf7a3c9ce62d
OrdonioSa <sascha.ordonio@tt-s.com>
2021-06-15 15:45:11 +0200

    added support for path-style S3-HTTPS-URLs

--- copy_files/function.py
+++ copy_files/function.py
@@ -30,17 +30,6 @@ def handler(event, context):
 
 
 def download_file_data(presigned_url):
-    # https://tts-cloud-artifacts-529985782713-eu-west-1.s3.amazonaws.com/tts-cloud/build-artifact-consumer/dev/40/packaged.yaml?AWSAccessKeyId=ASIAXWZM5IO45YFET2IG&Signature=8JT2VOwnzLe8%2FQ3bqbZsWQp3hRg%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEGQaCWV1LXdlc3QtMSJIMEYCIQCjBab4xy4sJYyRXdxViiXJCUK8pB0%2BANDyd9myr6LpyAIhAOZNcgmYGYDcviySF91N5nyA55waX7eoNYoty4Bl46eiKsECCC0QABoMNTI5OTg1NzgyNzEzIgw4GSC6Q5NpUBMbZ8sqngKun3orUbW1EoaxcSS5Gamsma16Bdd9DbRSHWEBB8MqYdDL5lsmJVvc01Js4WlFTCB%2BTg2PhU7BeVfB1GkH4uZT%2BYlJwTVPfatbThp6EKIokHZ3vdC7viaVQ1cP%2BB9G2sc8GH%2B2QYCt0FKjUab60Flbw%2BaLx3LMNXYld1PyMsvcUaciQmHdqh7dqVdj2SVt75WzWRpyUWbsCELWpuFJX9cmRxeSy9v%2FSFztsRB2H7b9Tcpa1mvuVG5QRs1yV1UQPcFqyi%2B2ZZGQL5DcH2HFfbyyoK0YesT9hcxj34xkem2xKxHVOWqUGKyJdIFVDJoFHUPRXfK2Pwg7buZvhOgt46qTZnLGaBBMvDuSPqNpFDZHXapNRH9MX9LGO%2FfAHuQoMPumooYGOpkBJ1AtSKLm%2FZW%2BZ5YL2coVyvWCDJYUDDVRAXbCJ7wygw1vyb7BLWzNXeBWkGc5AGqef3My2A5%2BHbUBgopSm2x1yrbY33Wo48UJ7QKYsZAOwPbuTXj88k9U%2BXuQarjBiYPy47KFtgdFzeQqCwZMl4DJg%2FJCRlwPFh1aVoud9NbbxS95EKlJZGvtcHGohj8z3jeZqkGGzSUF71PR&Expires=1623766459
-
-    """
-    requested_object_as_stream = requests.get(presigned_url, stream=True)
-    if int(requested_object_as_stream.status_code) >= 400:
-        raise IOError(f"Status: {requested_object_as_stream.status_code}, "
-                      f"{requested_object_as_stream.content.decode('utf-8')}")
-
-    return requested_object_as_stream.content
-    """
-
     parsed_url = urlparse(presigned_url)
     connection = http.client.HTTPSConnection(host=parsed_url.hostname, port=parsed_url.port)
     connection.request("GET", f"{parsed_url.path}?{parsed_url.query}")
